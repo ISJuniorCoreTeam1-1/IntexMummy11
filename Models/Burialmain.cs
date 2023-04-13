c6f83baaf7b7e99cfe4edd9faa31ff04fefc187e
Mitch McCoard <mitch.mccoard@gmail.com>
2023-04-13 17:44:33 -0600

    I'm so done right now

--- Models/Burialmain.cs
+++ Models/Burialmain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -9,6 +10,8 @@ namespace IntexMummy11.Models
 {
     public partial class Burialmain
     {
+        [Key]
+        [Required]
         public long Id { get; set; }
         public string Squarenorthsouth { get; set; }
         public string Headdirection { get; set; }
